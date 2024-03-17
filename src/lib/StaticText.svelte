995f4122c9f847cd3016ed8599b2dea4a2a4070e
SlowArmoredWarrior <slowarmoredwarrior99@gmail.com>
2024-03-17 10:46:42 -0400

    filename change <from StaticMessage>

---
+++ src/lib/StaticText.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { v4 } from "uuid";
+
+  import { simpleFillMethod } from "../utils/dom/fillmethods";
+
+  export let id = v4();
+  export let customClassList = "text-balance";
+
+  export let contents: string;
+
+  let thisNode: HTMLParagraphElement;
+
+  onMount(() => {
+    simpleFillMethod(thisNode, contents);
+  });
+</script>
+
+<p {id} class={customClassList} bind:this={thisNode} />
